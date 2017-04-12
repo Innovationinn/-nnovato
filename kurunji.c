e5af856a5a91ac29ec3f7e7f74a00c6d9e35a667
krreshma <reshmakr129@gmail.com>
2017-04-12 18:06:30 +0530

    Update kurunji.c

--- kurunji.c
+++ kurunji.c
@@ -2,7 +2,11 @@
 #include<conio.h>
 main()
 {
-int a;
-a=5*4
-printf("mull=",a);
+int a,b,c;
+printf("Enter 1 st no:");
+scanf("%d",&a);
+printf("Enter 2 nd no:");
+scanf("%d",&b);
+c=a*b;
+printf("mull=%d",c);
 }
