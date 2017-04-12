0f3e4f021efd590b009b787700742847725cb0ff
nandeshs <nandeshsasikumar@gmail.com>
2017-04-12 11:50:15 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,c;
+printf("Enter the first number:");
+scanf("%d",&a);
+printf("Enter the second number:")
+scanf("%d",&b);
+c=a+b;
+printf("Sum=",c);
+}
